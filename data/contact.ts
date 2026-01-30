955da349d157052e06a52f72d0e787333530df9f
Jose Vener Rafael <jose.rafael@jeonsoft.com>
2026-01-30 15:18:47 +0800

    Add resume pdf

--- data/contact.ts
+++ data/contact.ts
@@ -2,5 +2,5 @@ export const contact = {
   email: "rafaeljosevener@gmail.com",
   github: "https://github.com/josevener",
   linkedin: "https://linkedin.com/in/jose-vener-rafael-8541a7259",
-  resumeUrl: "/resume.pdf",
+  resumeUrl: "/josevener_resume.pdf",
 };
